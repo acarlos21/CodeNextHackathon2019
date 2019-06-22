de2a2e3debdd0fdc7dd529a99562546a37a3baeb
Masai'a <masaiaa@csedge.org>
2019-06-22 13:42:50 -0700

    lp

---
+++ joblist.js
@@ -0,0 +1,63 @@
+//
+let jobs = {
+  "one":{
+    "title": "Mobile App developer",
+    "Entry-level hourly wage":"$29.64",
+    "Typical qualifications" : "Associate's or Bachelor's degree",
+  },
+
+  "two":{
+    "title": "Wind Energy Technician",
+    "Entry-level hourly wage":"$27.88",
+    "Typical qualifications" : "Associate's",
+  },
+
+  "three":{
+    "title": "Wind Energy Technician",
+    "Entry-level hourly wage":"$18.40",
+    "Typical qualifications" : "Associate's degree",
+  },
+
+  "four":{
+    "title": "Web Designer",
+    "Entry-level hourly wage":"$18.24",
+    "Typical qualifications" : "Associate's degree or Bachelor's degree",
+  },
+
+  "five":{
+    "title": "Mechanical Engineering Technician",
+    "Entry-level hourly wage":"$16.78",
+    "Typical qualifications" : "Bachelor degree",
+  },
+
+  "six":{
+    "title": "Marketing Specialist",
+    "Entry-level hourly wage":"$16.50",
+    "Typical qualifications" : "Bachelor's degree",
+  },
+
+  "seven":{
+    "title": "Oil and Gas Derrick Operator",
+    "Entry-level hourly wage":"$16.01",
+    "Typical qualifications" : "High school diploma or Vocational certifate",
+  },
+
+  "eight":{
+    "title": "Electrian",
+    "Entry-level hourly wage":"$15.84",
+    "Typical qualifications" : "Vocational certifatea and paid apprenticeship",
+  },
+
+  "nine":{
+    "title": "Commercial Diver",
+    "Entry-level hourly wage":"$14.81",
+    "Typical qualifications" : "Scuba Certification and vocational certification",
+  },
+
+  "ten":{
+    "title": "Plumber",
+    "Entry-level hourly wage":"$15.43",
+  },
+}
+var job = jobs.one;
+ console.log(job.title);
